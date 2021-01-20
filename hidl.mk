707ff7b04f16e2901a202eceacf9eb8268e5f6b4
aymende7 <aymenbousselham@gmail.com>
2021-01-20 12:58:38 +0100

    freezerhd: Binderize healthd HIDL HAL

--- hidl.mk
+++ hidl.mk
@@ -44,6 +44,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service 
 
+# Healthd packages
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
