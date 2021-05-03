5cd946a778534a709bbd86465d88aca75872b355
R0rt1z2 <rogerortizleal@gmail.com>
2021-05-03 18:25:42 +0200

    freezerhd: releasetools: Set correct /dev/hdmitx permissions
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -2,3 +2,6 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/system", "/system");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system");');
+    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/vendor", "/vendor");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "s|    chmod 0660 /dev/hdmitx|    chmod 0666 /dev/hdmitx|g", "/vendor/etc/init/hw/init.mt8163.rc");');
+    info.script.AppendExtra('unmount("/vendor");');
