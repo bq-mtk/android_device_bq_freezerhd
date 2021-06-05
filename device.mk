86a0caed75a5a2d812050ab63e3db3cdc9089d30
tImIbreakdown <timifix@gmail.com>
2021-06-05 23:18:09 +0200

    freezerhd: Combine hidl.mk into device.mk
    Change-Id: Iff5caa2e284f8ac3453e3edeb4319a832ce06d84
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 LOCAL_PATH := device/bq/freezerhd
 DEVICE_FOLDER := device/bq/freezerhd
 
-# Hidl
-include $(LOCAL_PATH)/hidl.mk
-
 # Build Date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
@@ -122,6 +119,7 @@ PRODUCT_PACKAGES +=\
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
     hostapd_cli \
@@ -130,6 +128,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.r_submix.mt8163 \
     audio.usb.default \
     audio.a2dp.default \
@@ -146,8 +147,13 @@ PRODUCT_PACKAGES += \
     tinycap \
     tinyplay 
 
-# EGL
+# Graphics
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     libGLES_android
 
 # Net
@@ -164,8 +170,94 @@ PRODUCT_PACKAGES += \
     libcap \
     libqbaio_mono
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    libsensorndkbridge \
+    android.hardware.sensors@1.0-impl-mediatek \
+    android.hardware.sensors@1.0-service-mediatek
+
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+# DRM
+PRODUCT_PACKAGES += \
+    libdrm \
+    libmockdrmcryptoplugin \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
+# FM Radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.1-impl \
+    android.hardware.broadcastradio@1.1-service
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service 
+
+# Healthd packages
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service 
+
+# Light
+PRODUCT_PACKAGES += \
+    lights.mt8163 \
+    android.hardware.light@2.0-impl-mediatek \
+    android.hardware.light@2.0-service-mediatek
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0 \
+    android.hardware.thermal@1.1
+
+# USB 
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0 \
+    android.hardware.usb@1.0-service.basic \
+    android.hardware.usb.gadget@1.0-impl \
+    android.hardware.usb.gadget@1.0-service
+
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-impl \
+    android.hardware.configstore@1.0-service
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-mediatek
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+# HIDL Base
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl-mediatek \
+    android.hardware.bluetooth@1.0-service-mediatek \
     libbluetooth_mtk \
     libbt-vendor 
 
