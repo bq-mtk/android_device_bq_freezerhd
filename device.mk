6efce9d44539c586a2b4424cf814340d7f431ca6
jmpfbmx <jmpf.bmx@gmail.com>
2021-06-05 23:21:46 +0200

    freezerhd: device: Don't inherit full_base makefile
     * It is already being inherit by lineage makefile
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device.mk
+++ device.mk
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 LOCAL_PATH := device/bq/freezerhd
 DEVICE_FOLDER := device/bq/freezerhd
 
