bf289a965c37a5e63af38e2f831397e979f7a2f2
Dinolek <18579460+Dinolek@users.noreply.github.com>
2022-01-15 02:07:54 +0100

    freezerhd: patches: Import FMRadio Suppress compiler warnings
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ibf7f62cb418589ee29ef9c4bbea5716533bfde23

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ../../../..
+cd packages/apps/FMRadio
+git apply -v ../../../device/bq/freezerhd/patches/packages/apps/FMRadio/0001-Suppress-compiler-warnings.patch
+cd ../../..
 cd vendor/lineage
 git apply -v ../../device/bq/freezerhd/patches/vendor/lineage/0001-kernel-Don-t-use-build-image-kernel-modules-instead.patch
 cd ../..
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd ../../../..
+cd packages/apps/FMRadio
+git checkout -- . && git clean -df
+cd ../../..
 cd vendor/lineage
 git checkout -- . && git clean -df
 cd ../..
