4991484815cce3bffc7ef6817a55bab3091b4194
jmpfbmx <jmpf.bmx@gmail.com>
2022-01-15 02:10:02 +0100

    freezerhd: patches: Import Restore HW Rotation Properties
     * This fixes the screen rotation issue in BQ Aquaris M10 tablets.
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Iabee9f4acdc71e5d758d1deb11255c0d46b786fd

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ../../../..
+cd frameworks/native/
+git apply -v ../../device/bq/freezerhd/patches/frameworks/native/0001-Restore-HW-rotation-properties.patch
+cd ../..
 cd packages/apps/FMRadio
 git apply -v ../../../device/bq/freezerhd/patches/packages/apps/FMRadio/0001-Suppress-compiler-warnings.patch
 cd ../../..
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd ../../../..
+cd frameworks/native
+git checkout -- . && git clean -df
+cd ../..
 cd packages/apps/FMRadio
 git checkout -- . && git clean -df
 cd ../../..
