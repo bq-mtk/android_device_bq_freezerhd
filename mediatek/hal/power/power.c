a2bdd2b01f2d0ff985044fc88cafc0f3bbdc739a
Steve Kondik <shade@chemlab.org>
2022-01-15 17:15:42 +0100

    freezerhd: power: Add power hint to set profile
     * A PowerHAL can implement support for this hint to receive
       power profile changes from the framework.
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I10e5afb5b5fe65e1fe1578987a4a2c87696b31a3

--- mediatek/hal/power/power.c
+++ mediatek/hal/power/power.c
@@ -49,6 +49,7 @@ enum {
 #define HIGH_PERFORMANCE_PROP   "2"
 #define BIAS_POWER_PROP         "3"
 
+const static power_hint_t POWER_HINT_SET_PROFILE = (power_hint_t)0x00000111;
 static int current_power_profile = PROFILE_BALANCED;
 
 static void power_init(struct power_module *module)
