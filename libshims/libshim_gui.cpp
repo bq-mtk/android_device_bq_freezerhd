8fe1b7d9251aa986f84dfd5cfc2e80bbf4c06510
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-17 19:17:59 +0300

    freezerd: libshims: properly shim SurfaceComposerClient::createSurface()
    * The previous shim broke the function completely (even the parameters).
    
    * The following change invalidated all the function calls because the
      last two parameters of the function were changed (uint32 -> int32):
        - [O]: https://github.com/LineageOS/android_frameworks_native/blob/lineage-15.0/libs/gui/SurfaceComposerClient.cpp#L616
        - [P]: https://github.com/LineageOS/android_frameworks_native/blob/lineage-16.0/libs/gui/SurfaceComposerClient.cpp#L616
    
    * Fix it by casting the last two parameters.

--- libshims/libshim_gui.cpp
+++ libshims/libshim_gui.cpp
@@ -38,7 +38,14 @@ extern "C" void _ZN7android21SurfaceComposerClient20setDisplayProjectionERKNS_2s
   t.apply();
 }
 
-extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj() { }
+// android::SurfaceComposerClient::createSurface(android::String8 const&, unsigned int, unsigned int, int, unsigned int, android::SurfaceControl*, unsigned int, unsigned int)
+extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(android::String8 const & s, uint32_t w, uint32_t h,
+    android::PixelFormat format, uint32_t flags, void *parent, int32_t  windowType, int32_t ownerUid);
+
+// android::SurfaceComposerClient::createSurface(android::String8 const&, unsigned int, unsigned int, int, unsigned int, android::SurfaceControl*, int, int)
+extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(android::String8 const& s, uint32_t w, uint32_t h, android::PixelFormat fmt, uint32_t flags, void *parent, uint32_t windowType, uint32_t ownerUid) {
+    return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(s, w, h, fmt, flags, parent, static_cast<int32_t>(windowType), static_cast<int32_t>(ownerUid));
+}
 
 extern "C" void _ZN7android21SurfaceComposerClient21openGlobalTransactionEv() {
         t = new(android::SurfaceComposerClient::Transaction);
